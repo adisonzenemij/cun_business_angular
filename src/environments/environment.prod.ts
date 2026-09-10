8985ce5b2b872c3e3699b9fb6abb09e1c373e392
Adison Jimenez <adisonzenemij@gmail.com>
2026-09-10 01:31:10 -0500

    feat: f0f21a9c-1da4-4e10-a19d-f59e43657593

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: 'http://127.0.0.1:4159',
-  // apiUrl: 'https://cun-dis38.engcode.app',
+  // apiUrl: 'http://127.0.0.1:4159',
+  apiUrl: 'https://c4653888cea6.engcode.app',
   production: true,
   name: 'prod',
 };
--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: 'http://127.0.0.1:4159',
-  // apiUrl: 'https://cun-dis38.engcode.dev',
+  // apiUrl: 'http://127.0.0.1:4159',
+  apiUrl: 'https://c4653888cea6.engcode.dev',
   production: false,
   name: 'stag',
 };
