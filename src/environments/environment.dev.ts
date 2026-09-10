e9fb898eec5b5e25fd0d722a909657f89b16bdc1
Adison Jimenez <adisonzenemij@gmail.com>
2026-09-10 13:21:54 -0500

    feat: 001b6b69-acdb-4934-a437-a377b981738c

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,12 +1,12 @@
 export const environment = {
   // Backend Host
-  apiUrl: 'http://127.0.0.1:4159',
+  // apiUrl: 'http://127.0.0.1:4159',
 
   // Backend Docker
   // apiUrl: 'http://127.0.0.1:40159',
 
   // Backend Domain
-  // apiUrl: 'https://c4653888cea6.datacompute.org',
+  apiUrl: 'https://c4653888cea6.datacompute.org',
 
   production: false,
 };
